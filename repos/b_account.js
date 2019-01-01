55462d10120239ddd93b2af383e7349a9bda4a5a
1512006 <nttuananh7630@gmail.com>
2019-01-01 16:29:32 +0700

    Update transactionHistory: Hien thi mau account_number cua user

--- repos/b_account.js
+++ repos/b_account.js
@@ -44,13 +44,22 @@ class AccountRepos {
         return DbFunction.insert(sql);
     }
 
-    getTransactionHistory(uid) {
-        var sql = `SELECT distinct t.account_send, t.account_recieve, t.number_money, t.note, t.time from ${tableName} as b, ${tranactionTable} as t WHERE
-         (b.account_number = t.account_send or b.account_number = t.account_recieve)
-        and b.uid = ${uid}`;
+    // getTransactionHistory(uid) {
+    //     var sql = `SELECT distinct t.account_send, t.account_recieve, t.number_money, t.note, t.time, b.account_number from ${tableName} as b, ${tranactionTable} as t WHERE
+    //      (b.account_number = t.account_send or b.account_number = t.account_recieve)
+    //     and b.uid = ${uid}`;
 
+    //     return DbFunction.load(sql);
+    // }
+
+    getTransactionHistory(uid) {
+        var sql = `SELECT t.account_send, t.account_recieve, t.number_money, t.note, t.time, b1.account_number AS send, b2.account_number AS recieve
+        FROM ${tranactionTable} as t LEFT JOIN ${tableName} AS b1 ON t.account_send = b1.account_number AND b1.uid = ${uid} 
+        LEFT JOIN ${tableName} AS b2 ON t.account_recieve = b2.account_number AND b2.uid = ${uid}
+        WHERE b1.account_number IS NOT NULL or b2.account_number IS NOT NULL`
         return DbFunction.load(sql);
     }
+
     getAllAccountNumber(uid) {
         var sql = `SELECT account_number from ${tableName} where uid = ${uid}`;
         return DbFunction.load(sql);
